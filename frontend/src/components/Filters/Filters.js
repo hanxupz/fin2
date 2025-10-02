0fb5cebd7a5580d443cd2148c248ad1b11106757
hanxupz <ruithevvvv@gmail.com>
2025-10-02 03:55:53 +0100

    fix

--- frontend/src/components/Filters/Filters.js
+++ frontend/src/components/Filters/Filters.js
@@ -8,8 +8,6 @@ import {
   MenuItem,
   InputLabel,
   FormControl,
-  Grid,
-  Box,
   Stack
 } from "@mui/material";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
@@ -31,74 +29,62 @@ function Filters({
       <Typography variant="h6" sx={{ mb: 2, fontWeight: 600 }}>
         Filters
       </Typography>
-      <Grid container spacing={2}>
-        <Grid item xs={12} sm={6} md={3}>
-          <FormControl fullWidth size="small">
-            <InputLabel>Category</InputLabel>
-            <Select
-              label="Category"
-              value={filterCategory}
-              onChange={(e) => setFilterCategory(e.target.value)}
-            >
-              <MenuItem value=""><em>All</em></MenuItem>
-              {categories.map(c => <MenuItem key={c} value={c}>{c}</MenuItem>)}
-            </Select>
-          </FormControl>
-        </Grid>
-        <Grid item xs={12} sm={6} md={3}>
-          <FormControl fullWidth size="small">
-            <InputLabel>Account</InputLabel>
-            <Select
-              label="Account"
-              value={filterAccount}
-              onChange={(e) => setFilterAccount(e.target.value)}
-            >
-              <MenuItem value=""><em>All</em></MenuItem>
-              {accounts.map(a => <MenuItem key={a} value={a}>{a}</MenuItem>)}
-            </Select>
-          </FormControl>
-        </Grid>
-        <Grid item xs={12} sm={6} md={3}>
-          <DatePicker
-            label="From"
-            value={filterDateFrom}
-            onChange={setFilterDateFrom}
-            slotProps={{ textField: { size: 'small', fullWidth: true } }}
-          />
-        </Grid>
-        <Grid item xs={12} sm={6} md={3}>
-          <DatePicker
-            label="To"
-            value={filterDateTo}
-            onChange={setFilterDateTo}
-            slotProps={{ textField: { size: 'small', fullWidth: true } }}
-          />
-        </Grid>
-        <Grid item xs={12} sm={6} md={3}>
-          <DatePicker
-            label="Control Date"
-            value={filterControlDate}
-            onChange={setFilterControlDate}
-            slotProps={{ textField: { size: 'small', fullWidth: true } }}
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <Stack direction="row" spacing={2} justifyContent="flex-end">
-            <Button
-              variant="outlined"
-              onClick={() => {
-                setFilterCategory("");
-                setFilterAccount("");
-                setFilterDateFrom(null);
-                setFilterDateTo(null);
-                setFilterControlDate(null);
-              }}
-            >
-              Reset
-            </Button>
-          </Stack>
-        </Grid>
-      </Grid>
+      <Stack spacing={2}>
+        <FormControl fullWidth size="small">
+          <InputLabel>Category</InputLabel>
+          <Select
+            label="Category"
+            value={filterCategory}
+            onChange={(e) => setFilterCategory(e.target.value)}
+          >
+            <MenuItem value=""><em>All</em></MenuItem>
+            {categories.map(c => <MenuItem key={c} value={c}>{c}</MenuItem>)}
+          </Select>
+        </FormControl>
+        <FormControl fullWidth size="small">
+          <InputLabel>Account</InputLabel>
+          <Select
+            label="Account"
+            value={filterAccount}
+            onChange={(e) => setFilterAccount(e.target.value)}
+          >
+            <MenuItem value=""><em>All</em></MenuItem>
+            {accounts.map(a => <MenuItem key={a} value={a}>{a}</MenuItem>)}
+          </Select>
+        </FormControl>
+        <DatePicker
+          label="From"
+          value={filterDateFrom}
+          onChange={setFilterDateFrom}
+          slotProps={{ textField: { size: 'small', fullWidth: true } }}
+        />
+        <DatePicker
+          label="To"
+          value={filterDateTo}
+          onChange={setFilterDateTo}
+          slotProps={{ textField: { size: 'small', fullWidth: true } }}
+        />
+        <DatePicker
+          label="Control Date"
+          value={filterControlDate}
+          onChange={setFilterControlDate}
+          slotProps={{ textField: { size: 'small', fullWidth: true } }}
+        />
+        <Stack direction="row" spacing={2} justifyContent="flex-end">
+          <Button
+            variant="outlined"
+            onClick={() => {
+              setFilterCategory("");
+              setFilterAccount("");
+              setFilterDateFrom(null);
+              setFilterDateTo(null);
+              setFilterControlDate(null);
+            }}
+          >
+            Reset
+          </Button>
+        </Stack>
+      </Stack>
     </Paper>
   );
 }
