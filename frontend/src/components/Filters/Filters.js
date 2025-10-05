809fa73be1378f65c3b60ca748ad3beb8661c61d
ruithevvvv@gmail.com <ruithevvvv@gmail.com>
2025-10-05 21:11:52 +0100

    fix container

--- frontend/src/components/Filters/Filters.js
+++ frontend/src/components/Filters/Filters.js
@@ -25,67 +25,62 @@ function Filters({
   const theme = useTheme();
 
   return (
-    <Paper elevation={2} sx={(t) => ({ ...surfaceBoxSx(t), p: 3 })}>
-      <Typography variant="h6" sx={{ mb: 2, fontWeight: 600 }}>
-        Filters
-      </Typography>
-      <Stack spacing={2} sx={(t)=>({ ...surfaceBoxSx(t), p: 3, background: t.palette.background.paper })}>
-        <FormControl fullWidth size="small">
-          <InputLabel>Category</InputLabel>
-          <Select
-            label="Category"
-            value={filterCategory}
-            onChange={(e) => setFilterCategory(e.target.value)}
-          >
-            <MenuItem value=""><em>All</em></MenuItem>
-            {categories.map(c => <MenuItem key={c} value={c}>{c}</MenuItem>)}
-          </Select>
-        </FormControl>
-        <FormControl fullWidth size="small">
-          <InputLabel>Account</InputLabel>
-          <Select
-            label="Account"
-            value={filterAccount}
-            onChange={(e) => setFilterAccount(e.target.value)}
-          >
-            <MenuItem value=""><em>All</em></MenuItem>
-            {accounts.map(a => <MenuItem key={a} value={a}>{a}</MenuItem>)}
-          </Select>
-        </FormControl>
-        <DatePicker
-          label="From"
-          value={filterDateFrom}
-          onChange={setFilterDateFrom}
-          slotProps={{ textField: { size: 'small', fullWidth: true } }}
-        />
-        <DatePicker
-          label="To"
-          value={filterDateTo}
-          onChange={setFilterDateTo}
-          slotProps={{ textField: { size: 'small', fullWidth: true } }}
-        />
-        <DatePicker
-          label="Control Date"
-          value={filterControlDate}
-          onChange={setFilterControlDate}
-          slotProps={{ textField: { size: 'small', fullWidth: true } }}
-        />
-        <Stack direction="row" spacing={2} justifyContent="flex-end">
-          <Button
-            variant="outlined"
-            onClick={() => {
-              setFilterCategory("");
-              setFilterAccount("");
-              setFilterDateFrom(null);
-              setFilterDateTo(null);
-              setFilterControlDate(null);
-            }}
-          >
-            Reset
-          </Button>
-        </Stack>
+    <Stack spacing={2} sx={(t)=>({ ...surfaceBoxSx(t), p: 3, background: t.palette.background.paper })}>
+      <FormControl fullWidth size="small">
+        <InputLabel>Category</InputLabel>
+        <Select
+          label="Category"
+          value={filterCategory}
+          onChange={(e) => setFilterCategory(e.target.value)}
+        >
+          <MenuItem value=""><em>All</em></MenuItem>
+          {categories.map(c => <MenuItem key={c} value={c}>{c}</MenuItem>)}
+        </Select>
+      </FormControl>
+      <FormControl fullWidth size="small">
+        <InputLabel>Account</InputLabel>
+        <Select
+          label="Account"
+          value={filterAccount}
+          onChange={(e) => setFilterAccount(e.target.value)}
+        >
+          <MenuItem value=""><em>All</em></MenuItem>
+          {accounts.map(a => <MenuItem key={a} value={a}>{a}</MenuItem>)}
+        </Select>
+      </FormControl>
+      <DatePicker
+        label="From"
+        value={filterDateFrom}
+        onChange={setFilterDateFrom}
+        slotProps={{ textField: { size: 'small', fullWidth: true } }}
+      />
+      <DatePicker
+        label="To"
+        value={filterDateTo}
+        onChange={setFilterDateTo}
+        slotProps={{ textField: { size: 'small', fullWidth: true } }}
+      />
+      <DatePicker
+        label="Control Date"
+        value={filterControlDate}
+        onChange={setFilterControlDate}
+        slotProps={{ textField: { size: 'small', fullWidth: true } }}
+      />
+      <Stack direction="row" spacing={2} justifyContent="flex-end">
+        <Button
+          variant="outlined"
+          onClick={() => {
+            setFilterCategory("");
+            setFilterAccount("");
+            setFilterDateFrom(null);
+            setFilterDateTo(null);
+            setFilterControlDate(null);
+          }}
+        >
+          Reset
+        </Button>
       </Stack>
-    </Paper>
+    </Stack>
   );
 }
 
