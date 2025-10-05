61b7285abbca321bb532cb2ebb68311d0331dd95
ruithevvvv@gmail.com <ruithevvvv@gmail.com>
2025-10-05 21:07:24 +0100

    fix import

--- frontend/src/components/Credits/CreditsAccordion.js
+++ frontend/src/components/Credits/CreditsAccordion.js
@@ -7,6 +7,7 @@ import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
 import AddIcon from '@mui/icons-material/Add';
+import { surfaceBoxSx } from "../../theme/primitives";
 
 const formatDate = (d) => d ? new Date(d).toISOString().split('T')[0] : '';
 
